3717eaa8671c6bc05891341d0063552422ee4e85
ghkddlsgn <121390479+ghkddlsgn@users.noreply.github.com>
2026-04-06 12:17:19 +0200

    updating fastapi

---
+++ Backend_FastAPI/TodoApp/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+username = os.getenv("POSTGRES_USERNAME")
+password = os.getenv("POSTGRES_PASSWORD")
+host = os.getenv("POSTGRES_HOSTNAME")
+port = os.getenv("POSTGRES_PORT")
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+asyncpg://{username}:{password}"
+    f"@{host}:{port}/todos"
+)
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+Base = declarative_base()
---
+++ Backend_FastAPI/TodoApp/models.py
@@ -0,0 +1,13 @@
+from Backend_FastAPI.TodoApp.database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title:Mapped[str] = mapped_column(String)
+    description:Mapped[str] = mapped_column(String)
+    priority:Mapped[int] = mapped_column(Integer)
+    complete:Mapped[bool] = mapped_column(Boolean, default=False)
+    
